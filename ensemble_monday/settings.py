910d4220d5a4c21df7b4152e05b20151934b66e7
sunnyw <vnvnchlrh@gmail.com>
2020-09-17 19:55:25 +0900

    testing settings.py

--- ensemble_monday/settings.py
+++ ensemble_monday/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
-    'updates.apps.UpdatesConfig',
+    'updates',
     'polls.apps.PollsConfig',
     'daemun.apps.DaemunConfig',
     'faq.apps.FaqConfig',
