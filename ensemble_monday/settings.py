13e79ed0d24a296b0e1f4a908aadb6fbf02dc46a
sunnyw <vnvnchlrh@gmail.com>
2020-09-17 23:52:28 +0900

    test1_add app in settings

--- ensemble_monday/settings.py
+++ ensemble_monday/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'myupdates.apps.MyupdatesConfig',
     #'updates.apps.UpdatesConfig',
     'polls.apps.PollsConfig',
     'daemun.apps.DaemunConfig',
---
+++ myupdates/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyupdatesConfig(AppConfig):
+    name = 'myupdates'
