0ff3d25223770bd1feb018c32855bfa6c20ac862
sunnyw <vnvnchlrh@gmail.com>
2022-01-05 22:27:16 +0900

    retry

--- ensemble_monday/settings.py
+++ ensemble_monday/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 
 if os.path.isfile(os.path.join(BASE_DIR, 'secrets.json')) == True:
@@ -42,7 +41,7 @@ if os.path.isfile(os.path.join(BASE_DIR, 'secrets.json')) == True:
 
 else:
     #### Heroku ####
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
